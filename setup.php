06c566268b7dbd7004501603699edd5adb2b46e8
CorneelSoete <corneel.soete@gmail.com>
2021-12-08 11:19:26 +0100

    initial commit

---
+++ setup.php
@@ -0,0 +1,11 @@
+<?php
+
+function plugin_version_conventions(){
+
+    return ['name'           => 'Conventions',
+            'version'        => '1.0.0',
+            'author'         => 'Corneel Soete',
+            'license'        => 'GPLv3+',
+            'homepage'       => 'https://github.com/CorneelSoete/conventions',
+            'minGlpiVersion' => '9.5'];
+}
